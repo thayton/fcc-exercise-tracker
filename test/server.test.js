232e930d9ff61365eaff6d464d74b9d7f95824ff
Matthew Todd Hayton <todd.hayton@gmail.com>
2018-07-02 15:56:39 -0400

    Test for exercise addition

--- test/server.test.js
+++ test/server.test.js
@@ -66,9 +66,49 @@ describe('POST /api/exercise/new-user', () => {
     });
 });
 
-// Test that new exercise addition works
+describe('POST /api/exercise/add', () => {
+    // Test that new exercise addition works
+    it('should create a new exercise', (done) => {
+        User.findOne({}).then((user) => {
+            request(app)
+                .post('/api/exercise/add')
+                .send({
+                    userId: user._id,
+                    description: 'This exercise should get added',
+                    duration: 2,
+                    date: '1999-09-09 EDT'
+                })
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body._id).toBe(user._id),
+                    expect(res.body.username).toBe(user.username),
+                    expect(res.body.description).toBe('This exercise should get added'),
+                    expect(res.body.duration).toBe(2),
+                    expect(res.body.date).toBe('Thu Sep 9 1999')
+                }).end((err, res) => {
+                    if (err) {
+                        return done(err);
+                    }
+
+                    Exercise.find({ '_user': user._id }).then((exercises) => {
+                        expect(exercises.length).toBe(1);
+                        expect(exercises[0].description).toBe('This exercise should get added');
+                        expect(exercises[0].duration).toBe(2);
+                        done();
+                    }).catch((e) => {
+                        done(e);
+                    });
+                });
+        }).catch((e) => {
+            done(e);
+        });
+    });
+    
+    // Test that adding exercise to non-existing user fails
+});
+
 // Test that getting exercise log works
+describe('POST /api/exercise/log', () => {
 
+});
 
-
-// Test that adding exercise to non-existing user fails
